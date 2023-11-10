6cdff2003963ed311836ce9865172a4ba992dd50
rgam151 <138254056+rgam151@users.noreply.github.com>
2023-11-10 17:33:53 -0300

    Add files via upload

---
+++ pythonrg.py
@@ -0,0 +1,29 @@
+import mysql.connector
+import pymysql
+import os
+
+try:
+    connection= mysql.connector.connect
+        host: 
+        post:
+        user:
+        password:
+        bd:
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
